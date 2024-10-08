034b6f9b575595cff7b8c9c9ecf617482ea567e2
Johnny Kava <107259412+johnnykava@users.noreply.github.com>
2024-10-08 18:38:23 +0000

    Adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home(){
+    return <h1>
+        Oi amor! Está é minha primeira página colocada no ar, com tudo feito a mão.<br />
+        Estou muito feliz e queria agradecer por todo apoio. Te amooooo 😍<br />
+        E se me ama da uma risadinha!<br />
+    </h1>;
+}
+
+export default Home;
