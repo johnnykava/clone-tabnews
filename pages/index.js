e1e7efeaa6b32c9382c4e3fb8733ba0463855e0a
Johnny Kava <107259412+johnnykava@users.noreply.github.com>
2024-10-09 19:38:46 +0000

    Atualiza a pagina principal

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ function Home(){
     return <h1>
         Oi amor! Está é minha primeira página colocada no ar, com tudo feito a mão.<br />
         Estou muito feliz e queria agradecer por todo apoio. Te amooooo 😍<br />
-        E se me ama da uma risadinha!<br />
-    </h1>;
+        E se me ama da uma risadinha!<br /><br />
+
+        <h4>Obs: só vou mostrar para você esse desafio sabado, então perdão pela enrolação haha.</h4>
+    </h1>
+
 }
 
 export default Home;
